60ee46f0b5965fdf040949b90206b595fbfcb071
Ctanhuawu <yuuka@ccwait.com>
2026-08-31 22:35:29 +0800

    chore: align Extra.java methods with upstream 12.9

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/Extra.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/Extra.java
@@ -2,15 +2,27 @@ package tw.nekomimi.nekogram;
 
 import org.telegram.messenger.BuildConfig;
 
+import java.util.List;
+
 import tw.nekomimi.nekogram.helpers.UserHelper;
+import tw.nekomimi.nekogram.helpers.remote.ConfigHelper;
 
 public class Extra {
 
     public static int APP_ID = BuildConfig.API_ID;
     public static String APP_HASH = BuildConfig.API_HASH;
-    public static String SENTRY_DSN = BuildConfig.SENTRY_DSN;
+
+    public static final String PLAYSTORE_APP_URL = "";
+
+    public static String WS_USER_AGENT = "";
+    public static String WS_CONN_HASH = "";
+    public static String WS_DEFAULT_DOMAIN = "";
+
+    public static String TWPIC_BOT_USERNAME = null;
 
     public static boolean FORCE_ANALYTICS = "play".equals(BuildConfig.BUILD_TYPE);
+
+    public static String TLV_URL = "";
 
     private static final UserHelper.BotInfo HELPER_BOT = new UserHelper.BotInfo() {
         @Override
@@ -25,17 +37,25 @@ public class Extra {
     };
 
     public static UserHelper.BotInfo getHelperBot() {
-        if (BuildConfig.HELPER_BOT_USERNAME == null) {
+        if (BuildConfig.HELPER_BOT_USERNAME == null || BuildConfig.HELPER_BOT_USERNAME.isEmpty()) {
             return null;
         }
         return HELPER_BOT;
+    }
+
+    public static UserHelper.UserInfoBot getUserInfoBot(boolean fallback) {
+        return null;
     }
 
     public static boolean isDirectApp() {
         return "release".equals(BuildConfig.BUILD_TYPE) || "debug".equals(BuildConfig.BUILD_TYPE);
     }
 
+    public static List<ConfigHelper.News> getDefaultNews() {
+        return null;
+    }
+
     public static boolean isTrustedBot(long id) {
-        return id == BuildConfig.HELPER_BOT_ID;
+        return id != 0 && id == BuildConfig.HELPER_BOT_ID;
     }
 }
